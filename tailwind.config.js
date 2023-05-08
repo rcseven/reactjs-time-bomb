3cd061632ebd432d28668012abdacc9d3155b937
rcseven <rcedquila@gmail.com>
2023-05-09 02:26:31 +0800

    Configure Media Query to Desktop First

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,15 @@ export default {
       sm: "0.5em",
     },
     screens: {
-      sm: "480px",
-      // => @media (min-width: 640px) { ... }
+      sm: { max: "480px" },
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
+      md: { max: "768px" },
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+      lg: { max: "1024px" },
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
+      xl: { max: "1280px" },
 
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
+      "2xl": { max: "1536px" },
     },
     extend: {
       colors: {
